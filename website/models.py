c86cc896c746b119ec1ecfbe311c326ccfc47045
Kaan Tamer Yenideniz <ktyenideniz@yahoo.com>
2021-03-08 14:23:26 +0300

    changes to the CSS and HTML templates

--- website/models.py
+++ website/models.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 
 class Question(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=500)
+    text = models.TextField(help_text = 'Use $$...$$ format when entering mathematical equations. For example: $$x^2=4$$')
     created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
