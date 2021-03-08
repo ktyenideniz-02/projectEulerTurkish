acd358fa7e458e55ac17b190e3dc3d722c8d197c
Kaan Tamer Yenideniz <ktyenideniz@yahoo.com>
2021-03-08 08:04:36 +0300

    added single question page

--- website/views.py
+++ website/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 def question_list(request):
         questions = Question.objects.all()
         return render(request, 'website/question_list.html', {'questions' : questions})
+
+def question_detail(request, pk):
+        question = get_object_or_404(Question, pk=pk)
+        return render(request, 'website/question_detail.html', {'question': question})
